2a21ea91acd3c46aaab56ee4b703346a49820606
Daniel Song <daniel.skc@gmail.com>
2024-07-26 19:51:59 +1000

    Update Program.cs

--- di/Health.HBSP.Integration.ConsoleApp/Program.cs
+++ di/Health.HBSP.Integration.ConsoleApp/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Health.HBSP.Integration.Business;
+using Health.HBSP.Integration.ServiceCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Health.HBSP.Integration.ConsoleApp
 {
@@ -13,33 +16,116 @@ namespace Health.HBSP.Integration.ConsoleApp
                 var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
 
                 // do work here / get your work service ...
+                var _worker = host.Services.GetService<ITestInterface>();
+                _worker.Foo();
+
 
                 lifetime.StopApplication();
                 await host.WaitForShutdownAsync();
             }
             // await CreateHostBuilder(args).RunConsoleAsync();
-            //ar host = CreateHostBuilder(args).Build();
+            // var host = CreateHostBuilder(args).Build();
         }
 
-        //private static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .UseConsoleLifetime()
-        //        .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Warning))
-        //        .ConfigureServices((hostContext, services) =>
-        //        {
-        //            services.Configure<MyServiceOptions>(hostContext.Configuration);
-        //            services.AddHostedService<MyService>();
-        //            services.AddSingleton(Console.Out);
-        //        });
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        private static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .UseConsoleLifetime()
+            .ConfigureLogging(builder => {
+                builder.SetMinimumLevel(LogLevel.Debug);
+                builder.AddConsole();
+            })
             .ConfigureServices((hostContext, services) =>
             {
                 // remove the hosted service
                 // services.AddHostedService<Worker>();
 
                 // register your services here.
+                services.AddScoped<IPortalSubmission, PortalSubmission>();
+                services.AddScoped<IAzureBlobStorageService, AzureBlobStorageService>();
+                services.AddScoped<ServiceFramework.IDataverseService, ServiceFramework.DataverseService>();
+                services.AddScoped<ITestInterface, TestClass>();
+                services.AddHostedService<ConsoleHostedService>();
             });
+    }
+
+    internal sealed class ConsoleHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IHostApplicationLifetime _appLifetime;
+
+        public ConsoleHostedService(
+            ILogger<ConsoleHostedService> logger,
+            IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+
+            _appLifetime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(async () =>
+                {
+                    try
+                    {
+                        _logger.LogInformation("Hello World!");
+
+                        // Simulate real work is being done
+                        await Task.Delay(1000);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Unhandled exception!");
+                    }
+                    finally
+                    {
+                        // Stop the application once the work is done
+                        _appLifetime.StopApplication();
+                    }
+                });
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    internal class Worker : IHostedService
+    {
+        public Worker(ITestInterface testClass)
+        {
+            testClass.Foo();
+        }
+
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ITestInterface
+    {
+        void Foo();
+    }
+
+    public class TestClass : ITestInterface
+    {
+        public void Foo()
+        {
+            Console.WriteLine("Foo");
+        }
     }
 }
