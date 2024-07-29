b215f7ae9aaedef4b87505802662d311cfb5d11f
Daniel Song <daniel.skc@gmail.com>
2024-07-29 20:38:00 +1000

    Update IAzureBlobStorageService.cs

--- di/Health.HBSP.Integration.ServiceCore/AzureBlobStorage/IAzureBlobStorageService.cs
+++ di/Health.HBSP.Integration.ServiceCore/AzureBlobStorage/IAzureBlobStorageService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,7 @@ namespace Health.HBSP.Integration.ServiceCore
 {
     public interface IAzureBlobStorageService
     {
+        string GetBlob(string key);
+        bool SaveBlob(string key, string value);
     }
 }
