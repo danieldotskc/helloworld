6c43718c2a6267bf75720c800a36b6ca8d8fcddd
Daniel Song <daniel.skc@gmail.com>
2024-07-29 20:37:32 +1000

    Update DataverseService.cs

--- di/Health.HBSP.Integration.ServiceFramework/Dataverse/DataverseService.cs
+++ di/Health.HBSP.Integration.ServiceFramework/Dataverse/DataverseService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,13 @@ namespace Health.HBSP.Integration.ServiceFramework
 {
     public class DataverseService : IDataverseService
     {
+        public string Retrieve(string Id)
+        {
+            return $"Dataverse record of {Id}";
+        }
+        public bool Create(string value)
+        {
+            return true;
+        }
     }
 }
